fa16c1f935a3ca008eff27f54f1e4b518f5d5303
Igor Wertepny <ikwertepny@gmail.com>
2023-10-28 11:30:06 +0200

    Added files to handle different actions, user and files handling and also GUI actions.

---
+++ user_handling.py
@@ -0,0 +1,7 @@
+def create_user(login, password, email):
+    #This function is set to create a new user. It will ask for login, password and email to create a new acc.
+    #Next it should save it in text file with password protection into two different dictionaries: login + password
+    #and into login + email. First dict will be used to check user during login and later, with requested files
+    #changes. Second will be used to additionally verify user during critical actions.
+    pass
+
