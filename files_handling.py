2752428712bd96f3cdd9f4124472e17d8d562646
Igor Wertepny <ikwertepny@gmail.com>
2023-10-28 14:29:57 +0200

    Welcome screen during preparation. To do: grid labels and entries correctly

--- files_handling.py
+++ files_handling.py
@@ -1,6 +1,39 @@
-import os
-
 
 #This file will be responsible for actions with files. Different functions of module os will help to create,
 #open, close etc. files between locations. In text files it will be also possible to open file in chosen
 #program (Word, Pages etc." and makes changes.
+
+
+from tkinter import *
+
+# button - you clik it, then it does stuff
+
+count = 0
+
+
+def click():
+    global count
+    count += 1
+    print(f"You clicked the button {count}times. ")
+
+
+window = Tk()
+
+# photo = PhotoImage(file=" ") # name of file or path to file OR image name if we past it in
+
+button = Button(window,
+                text="Click it!",
+                command=click,
+                font=("Comic Sans", 40),
+                fg="#00FF00",
+                bg="black",
+                activeforeground="#00FF00",
+                activebackground="black",
+                state=ACTIVE,
+                # image=photo,
+                # compound= "bottom",
+
+                )
+button.pack()
+
+window.mainloop()
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-# import os
-# from tkinter import *
-#
-#
-#
-# entryWindow = Tk()
-#
-# entry1 = Entry(entryWindow)
-#
-# entryWindow.mainloop()
+import os
+from tkinter import *
+
+
+loginWindow = Tk()
+loginWindow.geometry("1000x600")
+loginWindow.title("File Manager")
+loginWindow.config(background="black")
+
+welcomeLable = Label(loginWindow, text="Welcome in File Manager!", font=("Arial", 30, "bold"),
+                     foreground='white', background='black', relief=SUNKEN, bd=10, padx=40, pady=40,
+                     compound='top')
+welcomeLable.pack()
+
+
+loginEntry = Entry(loginWindow, font=("Arial", 30, "bold"), background='black', foreground='white')
+loginEntry.grid(row=0, column=1)
+
+passwordEntry = Entry(loginWindow, font=("Arial", 30, "bold"), background='black',foreground='white', show="*")
+passwordEntry.grid(row=1, column=1)
+
+loginLabel = Label(loginWindow, text="Login: ")
+loginLabel.grid(row=0, column=0)
+
+passwordLabel = Label(loginWindow, text="Password: ")
+passwordLabel.grid(row=1, column=0)
+
+loginButton = Button(loginWindow, text="Login", bd=5, padx=5, pady=5)
+loginButton.pack()
+
+loginWindow.mainloop()
