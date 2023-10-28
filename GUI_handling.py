7c2cab4993796927fee24cb9216ca213aa6e1f56
Igor Wertepny <ikwertepny@gmail.com>
2023-10-28 15:45:15 +0200

    Logging screen done, login button opens new window. To do: resolve issue with login button and turn it off after one click

--- GUI_handling.py
+++ GUI_handling.py
@@ -1,3 +1,6 @@
 #This file will be used to create windows with proper buttons and entry spaces, to handle different
 #requirments due to different user actions
+from tkinter import *
+
+
 
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import os
 from tkinter import *
+from GUI_handling import *
+
+def new_window():
+
+    new_window = Toplevel()
+    # loginWindow.destroy()
+
+    new_window.title("Login passed")
+    new_window.geometry("1000x600")
+    new_window.config(background="black")
+
+    passed_Label = Label(new_window, text="PASSED", relief=SUNKEN)
+    passed_Label.pack()
+
+    new_window.mainloop()
 
 
 loginWindow = Tk()
@@ -8,24 +23,26 @@ loginWindow.title("File Manager")
 loginWindow.config(background="black")
 
 welcomeLable = Label(loginWindow, text="Welcome in File Manager!", font=("Arial", 30, "bold"),
-                     foreground='white', background='black', relief=SUNKEN, bd=10, padx=40, pady=40,
+                     foreground='white', background='black', relief=RAISED, bd=10, padx=40, pady=40,
                      compound='top')
 welcomeLable.pack()
 
+loginFrame = Frame(loginWindow)
+loginFrame.pack()
 
-loginEntry = Entry(loginWindow, font=("Arial", 30, "bold"), background='black', foreground='white')
+loginEntry = Entry(loginFrame, font=("Arial", 30, "bold"), background='black', foreground='white')
 loginEntry.grid(row=0, column=1)
 
-passwordEntry = Entry(loginWindow, font=("Arial", 30, "bold"), background='black',foreground='white', show="*")
+passwordEntry = Entry(loginFrame, font=("Arial", 30, "bold"), background='black',foreground='white', show="*")
 passwordEntry.grid(row=1, column=1)
 
-loginLabel = Label(loginWindow, text="Login: ")
+loginLabel = Label(loginFrame, text="Login: ")
 loginLabel.grid(row=0, column=0)
 
-passwordLabel = Label(loginWindow, text="Password: ")
+passwordLabel = Label(loginFrame, text="Password: ")
 passwordLabel.grid(row=1, column=0)
 
-loginButton = Button(loginWindow, text="Login", bd=5, padx=5, pady=5)
+loginButton = Button(loginWindow, text="Login", bd=5, padx=5, pady=5, command=new_window)
 loginButton.pack()
 
 loginWindow.mainloop()
